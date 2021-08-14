d0c7ae1739cfe16c8339e8f25bc3e226a94b42f4
Eljos Paul <eljospaul@gmail.com>
2021-08-14 23:52:15 +0530

    Changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -28,16 +28,16 @@ navLink.forEach(n => n.addEventListener('click', linkAction));
 /*===== SCROLL REVEAL ANIMATION =====*/
 const sr = ScrollReveal({
 origin: 'top',
-distance: '80px',
-duration: 2000,
-reset: true
+distance: '100px',
+duration: 1000,
+reset: false
 });
 
 /*SCROLL HOME*/
 sr.reveal('.home__title',{}); 
-sr.reveal('.button',{delay: 200}); 
+sr.reveal('.button',{delay: 50}); 
 sr.reveal('.home__img',{delay: 400}); 
-sr.reveal('.home__social-icon',{ interval: 200}); 
+sr.reveal('.home__social-icon',{ interval: 100}); 
 
 /*SCROLL ABOUT*/
 sr.reveal('.about__img',{}); 
@@ -54,4 +54,4 @@ sr.reveal('.skills__img',{delay: 600});
 sr.reveal('.work__img',{interval: 200}); 
 
 /*SCROLL CONTACT*/
-sr.reveal('.contact__input',{interval: 200});
+sr.reveal('.contact__input',{interval: 0});
